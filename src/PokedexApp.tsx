514c4a67ea0e951ee099d58de3a865cdabd1e3a9
Juan Sebastian Usuga Agudelo <usuga1997@gmail.com>
2025-03-14 18:45:27 -0500

    feat: Initial project structure

---
+++ src/PokedexApp.tsx
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { StackNavigator } from './presentation/navigator/StackNavigator';
+
+export const PokedexApp = () => {
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
+};
