cc908026877ee75b269415965571ae9c6f7d890f
Juan Sebastian Usuga Agudelo <usuga1997@gmail.com>
2025-03-16 08:22:58 -0500

    feat: Do all the necessary configuration to get the list of pokemons

---
+++ src/actions/pokemons/get-pokemons.ts
@@ -0,0 +1,24 @@
+import { pokeApi } from '../../config/api/pokeApi';
+import type { Pokemon } from '../../domain/entities/pokemon';
+import type { PokeAPIPaginatedResponse, PokeAPIPokemon } from '../../infrastructure/interfaces/pokeapi.interfaces';
+import { PokemonMapper } from '../../infrastructure/mappers/pokemon.mapper';
+
+export const getPokemons = async (page: number, limit: number = 20): Promise<Pokemon[]> => {
+  try {
+    const url = `/pokemon?offset=${ page * 10 }&limit=${ limit }`;
+    const { data } = await pokeApi.get<PokeAPIPaginatedResponse>(url);
+
+    const pokemonPromises = data.results.map((info) => {
+      return pokeApi.get<PokeAPIPokemon>(info.url);
+    });
+
+    const pokeApiPokemons = await Promise.all(pokemonPromises);
+    const pokemons = pokeApiPokemons.map(item =>
+      PokemonMapper.pokeApiPokemonToEntity(item.data)
+    );
+
+    return pokemons;
+  } catch (error) {
+    throw new Error('Error getting pokemons');
+  }
+};
--- src/presentation/screens/Home/HomeScreen.tsx
+++ src/presentation/screens/Home/HomeScreen.tsx
@@ -1,15 +1,46 @@
+/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
-import { Text, View } from 'react-native';
-import { Button } from 'react-native-paper';
+import { FlatList, StyleSheet, View } from 'react-native';
+import { getPokemons } from '../../../actions/pokemons';
+import { useQuery } from '@tanstack/react-query';
+import { PokeballBg } from '../../components/ui/PokeballBg';
+import { Text } from 'react-native-paper';
+import { globalTheme } from '../../../config/theme/global-theme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { PokemonCard } from '../../components/pokemons/PokemonCard';
 
 export const HomeScreen = () => {
-  return (
-    <View>
-      <Text>Home Screen!!!</Text>
+  const { top } = useSafeAreaInsets();
+  const { isLoading, data: pokemons = [] } = useQuery({
+    queryKey: [ 'pokemon' ],
+    queryFn: () => getPokemons(0),
+    staleTime: 1000 * 60 * 60,
+  });
 
-      <Button mode="contained" onPress={() => console.log('Pressed')}>
-        Press me
-      </Button>
+  return (
+    <View style={globalTheme.globalMargin}>
+      <PokeballBg style={styles.imgPosition} />
+
+      <FlatList
+        data={pokemons}
+        keyExtractor={(pokemon, index) => `${ pokemon.id }-${ index }`}
+        numColumns={2}
+        style={{
+          paddingTop: top + 20,
+        }}
+        ListHeaderComponent={() => (
+          <Text variant="displayMedium">Pokédex</Text>
+        )}
+        renderItem={({ item }) => <PokemonCard pokemon={item} />}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  imgPosition: {
+    position: 'absolute',
+    top: -100,
+    right: -100,
+  },
+});
